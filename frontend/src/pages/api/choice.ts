1436d46885f5d4d43b771038f89600d4d1471882
apemon <760108+apemon@users.noreply.github.com>
2023-11-18 02:07:28 +0300

    add api for choice

--- frontend/src/pages/api/choice.ts
+++ frontend/src/pages/api/choice.ts
@@ -1,44 +1,79 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {seedToChoices} from '@/utils/seed'
+import { Hex, toHex } from 'viem'
 import {OpenAI} from 'openai'
 
-type Data = {
-  name: string
+interface Story {
+  title: string
+  description: string
+  yes: string
+  no: string
+  yesStat: string
+  noStat: string
 }
 
+// GET /api/choice?seed=xxx&chainId=xxx
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Story[]>
 ) {
-  const theme = `autumn brown-ish medieval`
-  const openai = new OpenAI();
-  const response = await openai.chat.completions.create({
-    messages: [{
-      role:'system',
-      content: `You are a story writing agent for role-playing game. The player will be give the role of the king ot the country. The country theme will be ${theme}. The story you write will be a short story for player to make decision with yes/no choice. Each story choice will affect the stat of the player country. The story should have  no longer than 25 words. All stat will have value 0 to 100. The more value in each stat will mean that your country is better in that aspect. The stat will have as follow:
-      - gold, is the stat of your country wealth
-      - science, is the stat of your country technology
-      - religion, is the stat of your country faith & religion
-      - nature, is the stat of your country natural resources
-      There are some rules that you need to follow:
-      - do not mention about that player is a king of the country
-      - do not mention about the country background story
-      - do not include the +/- of stat of the yes/no choice
-      - add some the short description of the outcome of the yes/no choice. it should have no longer than 10 words`
-      
-    },{
-      role: 'user',
-      content: `Please write a story that have a yes/no choice that will affect the stat of the player country as the following stat
-      - yes: +10 science, -10 nature
-      - no: -10 science, +10 nature`
-    }],
-    model: 'gpt-3.5-turbo-16k-0613',
-    temperature: 0.5
-  })
-  console.log(response.choices[0].message)
-
-  if (req.method == 'POST') {
-    // xxx 
+  if (req.method != 'GET'){
+    return res.status(404)
   }
-  res.status(200).json(response.choices[0].message)
+  const seed = req.query.seed as Hex
+  const chainId = req.query.chainId as string || ''
+  const theme = `autumn brown-ish medieval`
+  const points = seedToChoices(seed)
+  let stories:Story[] = new Array<Story>(5)
+  const openai = new OpenAI();
+  await Promise.all(points.map(async (point, index) => {
+    let yesStat = ''
+    let noStat = ''
+    point.plus.forEach((stat) => {
+      yesStat += `+1 ${stat} `
+      noStat += `-1 ${stat} `
+    })
+    point.minus.forEach((stat) => {
+      yesStat += `-1 ${stat} `
+      noStat += `+1 ${stat} `
+    })
+    console.log(yesStat)
+    console.log(noStat)
+    const response = await openai.chat.completions.create({
+      messages: [{
+        role:'system',
+        content: `You are a story writing agent for role-playing game. The player will be give the role of the citizen in the country. The country theme will be ${theme}. You will write short story for player to make decision with yes/no choice. Each story choice will affect the stat of the player country. The more value in each stat will mean that your country is better in that aspect. The stat will have as follow:
+        - Gold, is the stat of your country wealth
+        - Science, is the stat of your country technology
+        - Religion, is the stat of your country faith & religion
+        - Nature, is the stat of your country natural resources
+        There are some rules that you need to follow:
+        - story should have no longer than 25 words.
+        - do not mention about that player is a king of the country
+        - do not mention about the country background story
+        - add some the short description of the outcome of the yes/no choice. it should have no longer than 10 words. do not include the +/- stat in the outcome description
+        - You should ONLY response in the JSON format as described below:
+          - title: story title
+          - description: story description
+          - yes: yes choice outcome description
+          - no: no choice outcome description
+        }
+        do not try to beautify the json result`
+      },{
+        role: 'user',
+        content: `Please write a story that have a yes/no choice that will affect the stat of the player country as the following stat
+        - yes: ${yesStat}
+        - no: ${noStat}`
+      }],
+      model: 'gpt-3.5-turbo-16k-0613',
+      temperature: 0.5
+    })
+    stories[index] = {
+      ...JSON.parse(response.choices[0].message.content || ''),
+      yesStat: yesStat.trim(),
+      noStat: noStat.trim()
+    }
+  }))
+  res.status(200).json(stories)
 }
