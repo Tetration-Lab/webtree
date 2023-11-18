f00236e6e43ee433243379371c447b6947cec200
apemon <760108+apemon@users.noreply.github.com>
2023-11-18 13:21:57 +0300

    add retry & change to gpt-4

--- frontend/src/pages/api/choice.ts
+++ frontend/src/pages/api/choice.ts
@@ -7,6 +7,62 @@ import NodeCache from 'node-cache'
 import {Story} from '@/interfaces/story'
 
 const choiceCache = new NodeCache( { stdTTL: 120, checkperiod: 300 } );
+const openai = new OpenAI();
+
+async function writeStoryWithRetry(theme: string, yesStat: string, noStat: string, maxRetries: number) {
+    let lastError: any;
+
+    for (let attempt = 0; attempt < maxRetries; attempt++) {
+        try {
+            return await writeStory(theme, yesStat, noStat);
+        } catch (error) {
+            console.log(error)
+            lastError = error;
+            await new Promise(resolve => setTimeout(resolve, 0));
+        }
+    }
+
+    throw lastError;
+}
+
+async function writeStory(theme: string, yesStat: string, noStat: string): Promise<Story> {
+  const response = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `You are a story writing agent for role-playing game. The player will be give the role of the citizen in the country. The country theme will be ${theme}. You will write short story for player to make decision with yes/no choice. Each story choice will affect the stat of the player country. The more value in each stat will mean that your country is better in that aspect. The stat will have as follow:
+      - Gold, is the stat of your country wealth
+      - Science, is the stat of your country technology
+      - Religion, is the stat of your country faith & religion
+      - Nature, is the stat of your country natural resources
+      There are some rules that you need to follow:
+      - story should have no longer than 25 words.
+      - do not mention about the country background story
+      - add some the short description of the outcome of the yes/no choice. it should have no longer than 10 words. do not include the +/- stat in the outcome description
+      - You should ONLY response in the JSON format as described below:
+        - title: story title
+        - emoji: emoji that represent the story title
+        - description: story description. do not include +/- stat here
+        - yes: yes choice outcome description
+        - no: no choice outcome description
+      }
+      do not try to beautify the json result`,
+        },
+        {
+          role: "user",
+          content: `Please write a story that have a yes/no choice that will affect the stat of the player country as the following stat
+      - yes: ${yesStat}
+      - no: ${noStat}`
+    }],
+    model: 'gpt-4-1106-preview',
+    temperature: 0.8
+  })
+  return {
+    ...JSON.parse(response.choices[0].message.content || ''),
+    yesStat: yesStat.trim(),
+    noStat: noStat.trim()
+  }
+}
 
 // GET /api/choice?seed=xxx&chainId=xxx
 export default async function handler(
@@ -34,6 +90,7 @@ export default async function handler(
   }
   const seed = req.query.seed as Hex
   const chainId:number = parseInt(req.query.chainId as string)
+  console.log(seed, chainId)
   const theme:string = themeMap[chainId]
   const choiceKey = `${seed}_${chainId}`
   if (choiceCache.has(choiceKey)){
@@ -42,55 +99,22 @@ export default async function handler(
   }
   const points = seedToChoices(seed)
   let stories:Story[] = new Array<Story>(5)
-  const openai = new OpenAI();
   await Promise.all(
     points.map(async (point, index) => {
+      console.log(point)
       let yesStat = "";
       let noStat = "";
       point.plus.forEach((stat) => {
-        yesStat += `+${point.stats[statIndexMap[stat]]} ${stat} `;
-        noStat += `-${point.stats[statIndexMap[stat]]} ${stat} `;
+        const statPoint = parseInt(point.stats[statIndexMap[stat]].toString())
+        yesStat += `${parseInt(point.stats[statIndexMap[stat]].toString())} ${stat} `;
+        noStat += `${-1 * parseInt(point.stats[statIndexMap[stat]].toString())} ${stat} `;
       });
       point.minus.forEach((stat) => {
-        yesStat += `-${point.stats[statIndexMap[stat]]} ${stat} `;
-        noStat += `+${point.stats[statIndexMap[stat]]} ${stat} `;
+        yesStat += `${parseInt(point.stats[statIndexMap[stat]].toString())} ${stat} `;
+        noStat += `${-1 * parseInt(point.stats[statIndexMap[stat]].toString())} ${stat} `;
       });
-      const response = await openai.chat.completions.create({
-        messages: [
-          {
-            role: "system",
-            content: `You are a story writing agent for role-playing game. The player will be give the role of the citizen in the country. The country theme will be ${theme}. You will write short story for player to make decision with yes/no choice. Each story choice will affect the stat of the player country. The more value in each stat will mean that your country is better in that aspect. The stat will have as follow:
-        - Gold, is the stat of your country wealth
-        - Science, is the stat of your country technology
-        - Religion, is the stat of your country faith & religion
-        - Nature, is the stat of your country natural resources
-        There are some rules that you need to follow:
-        - story should have no longer than 25 words.
-        - do not mention about that player is a king of the country
-        - do not mention about the country background story
-        - add some the short description of the outcome of the yes/no choice. it should have no longer than 10 words. do not include the +/- stat in the outcome description
-        - You should ONLY response in the JSON format as described below:
-          - title: story title
-          - description: story description
-          - yes: yes choice outcome description
-          - no: no choice outcome description
-        }
-        do not try to beautify the json result`,
-          },
-          {
-            role: "user",
-            content: `Please write a story that have a yes/no choice that will affect the stat of the player country as the following stat
-        - yes: ${yesStat}
-        - no: ${noStat}`
-      }],
-      model: 'gpt-3.5-turbo-16k-0613',
-      temperature: 1.5
-    })
-    stories[index] = {
-      ...JSON.parse(response.choices[0].message.content || ''),
-      yesStat: yesStat.trim(),
-      noStat: noStat.trim()
-    }
+      const story = await writeStoryWithRetry(theme, yesStat, noStat, 3)
+      stories[index] = story
   }))
   choiceCache.set(choiceKey, stories)
   res.status(200).json(stories)
